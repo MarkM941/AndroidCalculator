5fec71aee082fbc9659b905bf73c1ccff3593af9
mmillstein <mmillstein@hubspot.com>
2015-12-08 20:26:50 -0500

    basic buggy calculator functions

--- app/src/main/java/com/personal/mmillstein/androidcalculator/MainActivity.java
+++ app/src/main/java/com/personal/mmillstein/androidcalculator/MainActivity.java
@@ -1,31 +1,44 @@
 package com.personal.mmillstein.androidcalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private CalculatorUtils calculatorUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.calculatorUtils = new CalculatorUtils();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void handleNumberButtonPress(View view) {
+        String calcString = calculatorUtils.addDigit(((Button) view).getText().toString());
+        TextView calculatorTextView = (TextView) findViewById(R.id.text_calculator);
+        calculatorTextView.setText(calcString);
+    }
+
+    public void handleOperationButtonPress(View view) {
+        String calcString = calculatorUtils.addOperation((CalculatorUtils.Operation.from(((Button) view).getText().toString())));
+        TextView calculatorTextView = (TextView) findViewById(R.id.text_calculator);
+        calculatorTextView.setText(calcString);
+    }
+
+    public void handleEqualsButtonPress(View view) {
+        String calcString = calculatorUtils.calculate();
+        TextView calculatorTextView = (TextView) findViewById(R.id.text_calculator);
+        calculatorTextView.setText(calcString);
     }
 
     @Override
